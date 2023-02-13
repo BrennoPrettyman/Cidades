0e796db930f02fc19ba4914384024d64ab29d630
Brenno Prettyman <brennoprettyman@gmail.com>
2023-02-13 00:12:23 -0300

    Primeira alteração

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Cidades;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Aqui estão os nomes das cidades do litoral sul de São Paulo");
+        Console.WriteLine();
+        Console.WriteLine("Mongaguá");
+        Console.WriteLine("Peruíbe");
+        Console.WriteLine("Itanhaém");
+        Console.WriteLine("Ilha Comprida");
+        Console.WriteLine("Iguapé");
+        Console.WriteLine("Cananéia");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Cidades
+
 Exercício EtecAB
+
+**Enunciado**:
+
+Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.
+
+**Executar**:
+
+//
+dotnet run
+//
